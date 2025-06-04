2f92b2a0983dce6352bb771a5860f76a694c3b10
tsumarios <mraciti96@gmail.com>
2025-06-04 13:04:36 +0200

    Add support to ollama for local run

--- utils/local_llm/llama.py
+++ utils/local_llm/llama.py
@@ -1,19 +1,35 @@
-from llama_cpp import Llama
 import os
 import requests
 
-class LlamaHandler():
-  def __init__(self, n_ctx=None):
-    self.model_path = "./utils/local_llm/model/"
+class LlamaHandler:
+  def __init__(self, n_ctx=None, backend="ollama", model=None, base_url=None):
+    self.backend = backend
     self.n_ctx = n_ctx
-    if not os.listdir:
-      self.__download_model()
+
+    if backend == "ollama":
+      from langchain.chat_models import init_chat_model
+      self.config = {
+        "model": model,
+        "base_url": base_url,
+      }
+      if n_ctx:
+        self.config["context_window"] = int(n_ctx)
+      self.llm = init_chat_model(**self.config)
+
+    elif backend == "llama_cpp":
+      from llama_cpp import Llama
+      self.model_path = "./utils/local_llm/model/"
+      self.n_ctx = n_ctx
+      if not os.listdir:
+        self.__download_model()
+      else:
+        self.model_path = self.model_path + os.listdir(self.model_path)[0]
+      if n_ctx:
+        self.llm = Llama(model_path=self.model_path, n_ctx=int(n_ctx))
+      else:
+        self.llm = Llama(model_path=self.model_path)
     else:
-      self.model_path = self.model_path + os.listdir(self.model_path)[0]
-    if n_ctx:
-      self.llm = Llama(model_path=self.model_path, n_ctx=int(n_ctx))
-    else:
-      self.llm = Llama(model_path=self.model_path)
+        raise ValueError("Unsupported backend: choose 'ollama' or 'llama_cpp'.")
 
   def __download_model(self):
     print("---=== * * * ===---")
@@ -40,14 +56,13 @@ class LlamaHandler():
 
   def do_threat_modeling(self, sentence):
     prompt = "Q: " + sentence
-    response = ""
     print("Processing your request, please wait...")
-    if self.n_ctx:
-      response = self.llm(prompt, max_tokens=0)
-    else:
-      response = self.llm(prompt)
-    response = response["choices"][0]["text"]
-    return response
-
-          
-
+    if self.backend == "ollama":
+      response = self.llm.invoke(prompt)
+      return response.content if hasattr(response, "content") else str(response)
+    elif self.backend == "llama_cpp":
+      if self.n_ctx:
+        response = self.llm(prompt, max_tokens=0)
+      else:
+        response = self.llm(prompt)
+        return response["choices"][0]["text"]
